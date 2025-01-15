94130cbb1e9ca4086f81ebc4f44f97130b787259
RML-Cambo <ltomusic69@gmail.com>
2025-01-15 09:46:03 +0700

    C

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,10 +15,10 @@ export const Header = () => {
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
-            <a className="block text-teal-600" href="#">
+            <Link className="block text-teal-600" to="/">
               <span className="sr-only">Home</span>
               <img src="../../../public/img/logo.png" alt="" className="w-14" />
-            </a>
+            </Link>
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
